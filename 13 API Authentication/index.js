cd89ce3f6269e6e07430243c8d5b994cdc61e470
DerNikJakom <jan.komnik@googlemail.com>
2024-04-19 12:32:24 +0200

    Reupload

--- 13 API Authentication/index.js
+++ 13 API Authentication/index.js
@@ -6,66 +6,67 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 const yourUsername = "jkomnik";
-const yourPassword = "komnik123";
-const yourAPIKey = "8088e9ea-9b6c-4557-aa0f-9888058b2597";
-const yourBearerToken = "6fe6fc2c-7c10-4fc3-a59f-11b80509256a";
+const yourPassword = "root";
+const yourAPIKey = "07b07f28-5202-415a-b94b-912ed411a4df";
+const yourBearerToken = "Bearer e9286bc1-2043-4754-9df0-345362a57d34";
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { content: "API Response." });
 });
 
 app.get("/noAuth", async (req, res) => {
+
   try {
     const result = await axios.get(API_URL + "/random");
-    res.render("index.ejs", { content: JSON.stringify(result.data) });
+    res.render("index.ejs", {content: JSON.stringify(result.data)});
   } catch (error) {
-    res.status(404).send(error.message);
+    res.status(404).send("Error:", error.message)
   }
 });
 
 app.get("/basicAuth", async (req, res) => {
+  
   try {
-    const result = await axios.get(
-      API_URL + "/all?page=2",
-      {},
-      {
-        auth: {
-          username: yourUsername,
-          password: yourPassword,
-        },
-      }
-    );
-    res.render("index.ejs", { content: JSON.stringify(result.data) });
+    const result = await axios.get(API_URL + "/all", {
+      auth: {
+        username: yourUsername,
+        password: yourPassword,
+      },
+    });
+    res.render("index.ejs", {content: JSON.stringify(result.data)})
   } catch (error) {
-    res.status(404).send(error.message);
+    res.status(404).send("Error:", error.message)
   }
+   
+  
 });
 
 app.get("/apiKey", async (req, res) => {
+
   try {
-    const result = await axios.get(API_URL + "/filter", {
+    const result = await axios.get(API_URL + "/filter?score=5", {
       params: {
-        score: 5,
         apiKey: yourAPIKey,
       },
     });
     res.render("index.ejs", { content: JSON.stringify(result.data) });
   } catch (error) {
-    res.status(404).send(error.message);
+    res.status(404).send("Error: ", error.message);
   }
 });
 
-const config = {
-  headers: { Authorization: `Bearer ${yourBearerToken}` },
-};
-
 app.get("/bearerToken", async (req, res) => {
+
   try {
-    const result = await axios.get(API_URL + "/secrets/2", config);
-    res.render("index.ejs", { content: JSON.stringify(result.data) });
-  } catch (error) {
-    res.status(404).send(error.message);
-  }
+  const result = await axios.get(API_URL + "/secrets/42", {
+    headers: { 
+      Authorization: yourBearerToken 
+    },
+  });
+  res.render("index.ejs", { content: JSON.stringify(result.data) });
+} catch (error) {
+  res.status(404).send("Error: ", error.message);
+}
 });
 
 app.listen(port, () => {
