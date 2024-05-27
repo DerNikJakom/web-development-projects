ca205fb45d41d26f47e143b49f0233afaad322b6
DerNikJakom <jan.komnik@googlemail.com>
2024-05-27 19:56:39 +0200

    Update

--- 20 Travel Tracker/index.js
+++ 20 Travel Tracker/index.js
@@ -19,58 +19,53 @@ app.use(express.static("public"));
 
 async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_countries");
-
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
   return countries;
 }
-
-function capitalize(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
-
 // GET home page
 app.get("/", async (req, res) => {
   const countries = await checkVisisted();
-  res.render("index.ejs", {
-    countries: countries,
-    total: countries.length,
-    // TODO: error: [String]
-  });
+  res.render("index.ejs", { countries: countries, total: countries.length });
 });
 
 //INSERT new country
 app.post("/add", async (req, res) => {
-  const input = capitalize(req.body.country.trim()); // ! Germany
+  const input = req.body["country"];
 
   try {
     const result = await db.query(
-      "SELECT country_code FROM countries WHERE country_name = $1", // ! DE
-      [input]
+      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+      [input.toLowerCase()]
     );
-    if (result.rows.length !== 0) {
-      const visitedCountry = await db.query(
-        "SELECT country_code FROM visited_countries WHERE country_code = $1",
-        [result.rows[0].country_code]
-      );
-      console.log(result.rows[0].country_code);
-    }
 
-  
-    if (result.rows.length !== 0) {
-      const data = result.rows[0];
-      const countryCode = data.country_code;
-  
-      await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)",
-      [countryCode]
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
       );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
     }
-    res.redirect("/");
   } catch (err) {
     console.log(err);
-    res.redirect("/");
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
   }
 });
 
