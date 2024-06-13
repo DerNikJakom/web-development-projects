5444a2c3c8a6c1f420bad4e997e58e0171e02e80
DerNikJakom <jan.komnik@googlemail.com>
2024-06-13 15:01:55 +0200

    update and delete file no longer needed

--- 32 Conditional Rendering Practice/src/components/Form.jsx
+++ 32 Conditional Rendering Practice/src/components/Form.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import Input from "./Input";
 
 function Form(props) {
   return (
     <form className="form">
-      <Input type="text" placeholder="Username" />
-      <Input type="password" placeholder="Password" />
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
       {!props.isRegistered && (
-        <Input type="password" placeholder="Confirm Password" />
+        <input type="password" placeholder="Confirm Password" />
       )}
       <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
     </form>
--- 32 Conditional Rendering Practice/src/components/Input.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Input(props) {
-  return <Input type={props.type} placeholder={props.placeholder} />;
-}
-
-export default Input;
