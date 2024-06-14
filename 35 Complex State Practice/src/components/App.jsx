404961f25cfa3a4ee82f0c78ca0f22ba0d50ff8e
DerNikJakom <jan.komnik@googlemail.com>
2024-06-14 19:20:17 +0200

    finish project

--- 35 Complex State Practice/src/components/App.jsx
+++ 35 Complex State Practice/src/components/App.jsx
@@ -4,8 +4,39 @@ function App() {
   const [contact, setContact] = useState({
     fName: "",
     lName: "",
-    email: ""
+    email: "",
   });
+
+  function changeHandler(event) {
+    const { value, name } = event.target;
+
+    setContact((prevValue) => {
+      // prevValue is equal to contact
+      const { fName, lName, email } = prevValue;
+      console.log(prevValue);
+      console.log(contact);
+
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: lName,
+          email: email,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: fName,
+          lName: value,
+          email: email,
+        };
+      } else {
+        return {
+          fName: fName,
+          lName: lName,
+          email: value,
+        };
+      }
+    });
+  }
 
   return (
     <div className="container">
@@ -14,9 +45,24 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input
+          onChange={changeHandler}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={changeHandler}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={changeHandler}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
         <button>Submit</button>
       </form>
     </div>
