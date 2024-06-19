4c92a3bcd1f1584b55af80c31d1ccdb5bc29ee4c
DerNikJakom <jan.komnik@googlemail.com>
2024-06-19 17:42:22 +0200

    finish project

--- 36 Spread Operator Practice/src/components/App.jsx
+++ 36 Spread Operator Practice/src/components/App.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [listItems, setListItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInput(newValue);
+  }
+
+  function handleClick() {
+    setListItems((prevValue) => {
+      return [...prevValue, input];
+    });
+    setInput("");
+  }
+
+  function getItems(item) {
+    return <li>{item}</li>;
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" onChange={handleChange} value={input} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
-        <ul>
-          <li>A Item</li>
-        </ul>
+        <ul>{listItems.map(getItems)}</ul>
       </div>
     </div>
   );
