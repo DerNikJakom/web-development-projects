2633b82586684f14a745894e88d1228a61517ec8
DerNikJakom <jan.komnik@googlemail.com>
2024-06-21 17:04:25 +0200

    finish project

--- 40 Styling the Keeper App/src/components/CreateArea.jsx
+++ 40 Styling the Keeper App/src/components/CreateArea.jsx
@@ -8,6 +8,7 @@ function CreateArea(props) {
     title: "",
     content: "",
   });
+  const [isExpanded, setIsExpanded] = useState(false);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -21,31 +22,43 @@ function CreateArea(props) {
   }
 
   function submitNote(event) {
+    event.preventDefault();
     props.onAdd(note);
     setNote({
       title: "",
       content: "",
     });
-    event.preventDefault();
+    setIsExpanded(false);
+  }
+
+  function expand() {
+    setIsExpanded(true);
   }
 
   return (
     <div>
       <form className="create-note">
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
         <textarea
           name="content"
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
+          onClick={expand}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
